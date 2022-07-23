f734697c3ec628f4c615123c716fc8e650e358ca
Kevin Tan <kevin2012tan@gmail.com>
2022-07-23 14:57:45 -0700

    Finished review class and finished getter methods

--- src/main/model/Review.java
+++ src/main/model/Review.java
@@ -7,19 +7,51 @@ public class Review {
 
     private Restaurant restaurant;
     private int rating;
+    private double cost;
     private String reviewComment;
 
     // EFFECTS: creates a review object for a restaurant with a given rating and review comment
-    public Review(Restaurant restaurant, int rating, String reviewComment) {
+    public Review(Restaurant restaurant, int rating, double cost, String reviewComment) {
         this.restaurant = restaurant;
         this.rating = rating;
+        this.cost = cost;
         this.reviewComment = reviewComment;
     }
 
+    // SPECIFIES: rating is a number between 1 and 5
+    // MODIFIES: this
+    // EFFECTS: updates rating for company to the new rating
+    public void changeRating(int rating) {
+        this.rating = rating;
+    }
 
-//      Fields:
-//      Restaurant Name
-//      Rating (out of 5)
-//      Methods:
-//      Edit review
+    // SPECIFIES: rating is a number between 1 and 5
+    // MODIFIES: this
+    // EFFECTS: updates rating for company to the new rating
+    public void changeReview(String reviewComment) {
+        this.reviewComment = reviewComment;
+    }
+
+    // SPECIFIES: rating is a number between 1 and 5
+    // MODIFIES: this
+    // EFFECTS: updates rating for company to the new rating
+    public void changeCost(double cost) {
+        this.cost = cost;
+    }
+
+    public Restaurant getRestaurant() {
+        return restaurant;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public String getReviewComment() {
+        return reviewComment;
+    }
 }
