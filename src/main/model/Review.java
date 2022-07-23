0d7496092a7859440701ddcee0073a72f5bc0ca4
Kevin Tan <kevin2012tan@gmail.com>
2022-07-23 11:23:52 -0700

    changed approach. now designing individual objects needed for the user component instead of writing a top level "User" class. Writing class level comments for Review and Reviewed restaurant class.

---
+++ src/main/model/Review.java
@@ -0,0 +1,12 @@
+package model;
+
+// A review object has the following properties:
+
+
+public class Review {
+//      Fields:
+//      Restaurant Name
+//      Rating (out of 5)
+//      Methods:
+//      Edit review
+}
---
+++ src/main/model/ReviewedRestaurants.java
@@ -0,0 +1,6 @@
+package model;
+
+// This is of Reviews class where we can add and delete restaurant reviews
+
+public class ReviewedRestaurants {
+}
