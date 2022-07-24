d2152e2f3eca233d34560a39a4a3c10473f5c056
Kevin Tan <kevin2012tan@gmail.com>
2022-07-24 12:02:02 -0700

    Created a user class, which documents a user writing a review, specifying their userID and userName

--- src/main/model/User.java
+++ src/main/model/User.java
@@ -2,5 +2,30 @@ package model;
 
 // User identifies the person writing a review
 
+import java.util.ArrayList;
+
 public class User {
+
+    private int userID;
+    private String userName;
+    private ArrayList<Review> myReviews;
+
+    // EFFECTS: creates a User object
+    public User(int userID, String userName) {
+        this.userID = userID;
+        this.userName = userName;
+        myReviews = new ArrayList<Review>();
+    }
+
+    // REQUIRES: userName is a non-empty string
+    // MODIFIES: this
+    // EFFECTS: changes the user's name
+    public void changeUserName(String userName) {
+        this.userName = userName;
+    }
+
+    // EFFECTS: returns a list of reviews made by the user
+    public ArrayList<Review> getMyReviews() {
+        return myReviews;
+    }
 }
