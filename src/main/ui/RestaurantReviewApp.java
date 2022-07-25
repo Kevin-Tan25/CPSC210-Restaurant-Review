fbfe908b58f72bc96f06d76f3f5b5e714892466c
Kevin Tan <kevin2012tan@gmail.com>
2022-07-24 17:36:50 -0700

    Created viewMyAccount function, which would allow users to manage their friends and view their reviews

--- src/main/ui/RestaurantReviewApp.java
+++ src/main/ui/RestaurantReviewApp.java
@@ -1,10 +1,15 @@
 package ui;
 
+import model.User;
+
 import java.util.Scanner;
 
 // Based off Teller app UI package TELLER APP class
 
 public class RestaurantReviewApp {
+
+    private User user;
+    private Scanner input;
 
     // EFFECTS: runs the Restaurant Review application
     public RestaurantReviewApp() {
@@ -37,14 +42,50 @@ public class RestaurantReviewApp {
     // MODIFIES: this
     // EFFECTS: processes user command
     private void processCommand(String command) {
-        if (command.equals("")) {
-            doDeposit();
-        } else if (command.equals("w")) {
-            doWithdrawal();
-        } else if (command.equals("t")) {
-            doTransfer();
+        if (command.equals("a")) {
+            viewMyAccount();
+//        } else if (command.equals("w")) {
+//            writeReview();
+//        } else if (command.equals("t")) {
+//            viewTopRestaurants();
         } else {
             System.out.println("Selection not valid...");
+        }
+    }
+
+    // MODIFIES: this
+    // EFFECTS: initializes user with existing reviews
+    private void init() {
+        user = new User(1,"Kevin");
+        input = new Scanner(System.in);
+        input.useDelimiter("\n");
+    }
+
+    // EFFECTS: displays menu of options to user
+    private void displayMenu() {
+        System.out.println("\nSelect from:");
+        System.out.println("\ta -> view account");
+//        System.out.println("\tw -> withdraw");
+//        System.out.println("\tt -> transfer");
+        System.out.println("\tq -> quit");
+    }
+
+    // EFFECTS: lets user view a list of their reviews, their username, or their friends
+    private void viewMyAccount() {
+        String selection = "";
+        System.out.println("Hello, " + user.getUserName() + "!\n");
+        System.out.println("\tm -> manage friends");
+        System.out.println("\tr -> view all reviews");
+        System.out.println("\tb -> return to home");
+        selection = input.next();
+        selection = selection.toLowerCase();
+
+        if (selection.equals("m")) {
+            manageFriends();
+        } else if (selection.equals("r")) {
+            displayReview();
+        } else {
+            System.out.println("Selection not valid");
         }
     }
 }
