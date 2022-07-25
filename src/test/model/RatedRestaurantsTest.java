3439f6193250e7083dd910f1708d3218cfbcda23
Kevin Tan <kevin2012tan@gmail.com>
2022-07-25 16:56:33 -0700

    Fixed test class for RatedRestaurant

--- src/test/model/RatedRestaurantsTest.java
+++ src/test/model/RatedRestaurantsTest.java
@@ -3,6 +3,8 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RatedRestaurantsTest {
@@ -12,6 +14,9 @@ public class RatedRestaurantsTest {
     Restaurant r1;
     Restaurant r2;
     Restaurant r3;
+    Review rev1;
+    Review rev2;
+    Review rev3;
 
     @BeforeEach
     public void setUp() {
@@ -22,6 +27,12 @@ public class RatedRestaurantsTest {
         r3 = new Restaurant("Five Guys", "4823 Thompson Ave");
         rr2.addRestaurant(r1);
         rr2.addRestaurant(r2);
+        rev1 = new Review(r1, 4, 3.99, "Good and cheap");
+        rev2 = new Review(r2, 3, 5.99, "Not bad");
+        rev3 = new Review(r3, 5, 8.45, "Lots of peanuts");
+        r1.addReview(rev1);
+        r2.addReview(rev2);
+        r3.addReview(rev3);
     }
 
     @Test
@@ -70,6 +81,15 @@ public class RatedRestaurantsTest {
         assertEquals(rr2.getRatedRestaurants().get(1), r1);
         assertEquals(rr2.getSortedRestaurant().size(),2);
         assertEquals(rr2.getSortedRestaurant().get(1), r1);
+    }
+
+    @Test
+    public void testGetTopRestaurants() {
+        rr2.addRestaurant(r3);
+        ArrayList<Restaurant> sortedList = rr2.getTopRestaurants();
+        assertEquals(sortedList.get(0), r3);
+        assertEquals(sortedList.get(1), r1);
+        assertEquals(sortedList.get(2), r2);
     }
 
 
