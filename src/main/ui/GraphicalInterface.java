bae69a8f13e5300f2c996f42b0cbf68fe3c9d09c
Kevin Tan <kevin2012tan@gmail.com>
2022-08-06 16:29:08 -0700

    Created splitpane view for application

--- src/main/ui/GraphicalInterface.java
+++ src/main/ui/GraphicalInterface.java
@@ -1,22 +1,77 @@
 package ui;
 
+import model.RatedRestaurants;
+import model.Restaurant;
+
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 // Represents the main window for restaurantReviewApp
 public class GraphicalInterface extends JFrame {
 
     // The dimensions of the frame of the application
-    public static final int WIDTH = 700;
+    public static final int WIDTH = 900;
     public static final int HEIGHT = 700;
 
+    private RatedRestaurants allLoggedRestaurants = new RatedRestaurants();
+
     private JFrame frameInterface;
+    private JList list = new JList();
+    private DefaultListModel model = new DefaultListModel();
     private GridBagConstraints gbc = new GridBagConstraints();
+    private JSplitPane splitPane = new JSplitPane();
 
     public GraphicalInterface() {
         super("Restaurant Review");
         initializeFrame();
+        initializeMenu();
+        initializeList();
+
+        splitPane.revalidate();
+    }
+
+    private void initializeMenu() {
+        JPanel layout = new JPanel(new BorderLayout());
+
+        JLabel welcomeMessage = getWelcomeMessage();
+        JPanel menuButtonLayout = new JPanel();
+        menuButtonLayout.setBorder(new EmptyBorder(5, 5, 5, 5));
+        JPanel buttonPane = new JPanel(new GridLayout(10, 1, 10, 5));
+        buttonPane.add(new JButton("View my account"));
+
+        JButton addReviewButton = new JButton("Write a review");
+        AddReviewListener addReview = new AddReviewListener(addReviewButton);
+        addReviewButton.addActionListener(addReview);
+        buttonPane.add(addReviewButton);
+
+        JButton viewTopRestaurantsButton = new JButton("View top restaurants");
+        ViewTopRestaurantsListener viewTopRestaurants = new ViewTopRestaurantsListener(viewTopRestaurantsButton);
+        addReviewButton.addActionListener(viewTopRestaurants);
+        buttonPane.add(viewTopRestaurantsButton);
+
+        buttonPane.add(new JButton("Search restaurant reviews"));
+        buttonPane.add(new JButton("Save reviews to file"));
+        buttonPane.add(new JButton("Load reviews from file"));
+        buttonPane.add(new JButton("Quit"));
+        menuButtonLayout.add(buttonPane);
+
+        layout.add(welcomeMessage, BorderLayout.NORTH);
+        layout.add(menuButtonLayout, BorderLayout.SOUTH);
+
+        splitPane.setLeftComponent(layout);
+        layout.revalidate();
+    }
+
+    private void initializeList() {
+        list.setModel(model);
+        for (Restaurant r: allLoggedRestaurants.getTopRestaurants()) {
+            model.addElement(r);
+        }
+        splitPane.setRightComponent(new JScrollPane(list));
+        list.revalidate();
     }
 
     // MODIFIES: this
@@ -30,31 +85,7 @@ public class GraphicalInterface extends JFrame {
         frameInterface.setLocationRelativeTo(null);
         frameInterface.setVisible(true);
         frameInterface.setBackground(new Color(180, 180, 180));
-
-        JLabel welcomeMessage = getWelcomeMessage();
-
-        JPanel buttonPane = getMenuPanel();
-
-        buttonPane.revalidate();
-        welcomeMessage.revalidate();
-    }
-
-    private JPanel getMenuPanel() {
-        JPanel layout = new JPanel(new GridBagLayout());
-        layout.setBorder(new EmptyBorder(5, 5, 5, 5));
-        JPanel buttonPane = new JPanel(new GridLayout(10, 1, 10, 5));
-        buttonPane.add(new JButton("View my account"));
-        buttonPane.add(new JButton("Write a review"));
-        buttonPane.add(new JButton("View top restaurants"));
-        buttonPane.add(new JButton("Search restaurant reviews"));
-        buttonPane.add(new JButton("Save reviews to file"));
-        buttonPane.add(new JButton("Load reviews from file"));
-        buttonPane.add(new JButton("Quit"));
-        layout.add(buttonPane, gbc);
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        frameInterface.add(layout, gbc);
-        return buttonPane;
+        frameInterface.add(splitPane);
     }
 
     private JLabel getWelcomeMessage() {
@@ -65,6 +96,33 @@ public class GraphicalInterface extends JFrame {
         frameInterface.add(welcomeMessage);
         return welcomeMessage;
     }
+
+    class AddReviewListener implements ActionListener {
+        private JButton button;
+
+        public AddReviewListener(JButton button) {
+            this.button = button;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+    class ViewTopRestaurantsListener implements ActionListener {
+        private JButton button;
+
+        public ViewTopRestaurantsListener(JButton button) {
+            this.button = button;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
 
     public static void main(String[] args) {
 
