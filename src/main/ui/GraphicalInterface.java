f1f65c714d2d72323dec947a5a736cd73c89f1da
Kevin Tan <kevin2012tan@gmail.com>
2022-08-06 11:03:53 -0700

    Created basic menu and welcome message for application

--- src/main/ui/GraphicalInterface.java
+++ src/main/ui/GraphicalInterface.java
@@ -8,36 +8,38 @@ import java.awt.*;
 public class GraphicalInterface extends JFrame {
 
     // The dimensions of the frame of the application
-    public static final int WIDTH = 1000;
+    public static final int WIDTH = 700;
     public static final int HEIGHT = 700;
 
     private JFrame frameInterface;
-    private MenuComponent menu;
+    private GridBagConstraints gbc = new GridBagConstraints();
 
     public GraphicalInterface() {
         super("Restaurant Review");
-        initializeGraphics();
-//        displayWelcomeMessage();
-    }
-
-    // EFFECTS: Greets the User
-    private void displayWelcomeMessage() {
-        JLabel welcomeMessage = new JLabel();
-        welcomeMessage.setText("Welcome to the Restaurant Review App! \n How can I help you today?");
-        frameInterface.add(welcomeMessage);
+        initializeFrame();
     }
 
     // MODIFIES: this
     // EFFECTS: creates the JFrame for the restaurant app
-    private void initializeGraphics() {
+    private void initializeFrame() {
 
-        JPanel layoutWelcome = new JPanel(new GridBagLayout());
-        layoutWelcome.setBorder(new EmptyBorder(5, 5, 5, 5));
-        JLabel welcomeMessage = new JLabel();
-        welcomeMessage.setText("Welcome to the Restaurant Review App! How can I help you today?");
-//        welcomeMessage.setBounds(10,0, 500, 50);
-        layoutWelcome.add(welcomeMessage);
+        frameInterface = new JFrame();
+        frameInterface.setLayout(new GridBagLayout());
+        frameInterface.setMinimumSize(new Dimension(WIDTH, HEIGHT));
+        frameInterface.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frameInterface.setLocationRelativeTo(null);
+        frameInterface.setVisible(true);
+        frameInterface.setBackground(new Color(180, 180, 180));
 
+        JLabel welcomeMessage = getWelcomeMessage();
+
+        JPanel buttonPane = getMenuPanel();
+
+        buttonPane.revalidate();
+        welcomeMessage.revalidate();
+    }
+
+    private JPanel getMenuPanel() {
         JPanel layout = new JPanel(new GridBagLayout());
         layout.setBorder(new EmptyBorder(5, 5, 5, 5));
         JPanel buttonPane = new JPanel(new GridLayout(10, 1, 10, 5));
@@ -48,29 +50,23 @@ public class GraphicalInterface extends JFrame {
         buttonPane.add(new JButton("Save reviews to file"));
         buttonPane.add(new JButton("Load reviews from file"));
         buttonPane.add(new JButton("Quit"));
-        layout.add(buttonPane);
+        layout.add(buttonPane, gbc);
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        frameInterface.add(layout, gbc);
+        return buttonPane;
+    }
 
-        frameInterface = new JFrame();
-        frameInterface.setLayout(new BorderLayout());
-        frameInterface.setMinimumSize(new Dimension(WIDTH, HEIGHT));
-
-//        welcomeMessage = new WelcomeMessage();
-//        menu = new MenuComponent();
-
-        frameInterface.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frameInterface.setLocationRelativeTo(null);
-        frameInterface.setVisible(true);
-        frameInterface.add(welcomeMessage, BorderLayout.NORTH);
-        frameInterface.add(layout, BorderLayout.WEST);
-        frameInterface.setBackground(new Color(180, 180, 180));
-
-        layoutWelcome.revalidate();
-        layout.revalidate();
+    private JLabel getWelcomeMessage() {
+        JLabel welcomeMessage = new JLabel();
+        welcomeMessage.setText("<html>Welcome to the Restaurant Review App!<br>How can I help you today?</html>");
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        frameInterface.add(welcomeMessage);
+        return welcomeMessage;
     }
 
     public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
 
         new GraphicalInterface();
     }
